c91db2891b44f3ef7011ce7b39208196db3810a3
Lee Huey Charn <hueycharn.lee@gmail.com>
2020-03-01 22:35:52 +0800

    Qs8.9

--- RecursionAndDP/Parens.java
+++ RecursionAndDP/Parens.java
@@ -4,16 +4,29 @@ public class Parens
 {
 	public static void main(String[] args)
 	{
-		Set<String> set = helper(6);
-		int i = 1;
+		//Set<String> set = helper(6);
+		//int i = 1;
 		/*for(String s : set)
 		{
 			System.out.println(i+ ": " + s);
 			i++;
 		}*/
+
+		List<String> list = new ArrayList();
+		int number =  2;
+		char[] array = new char[number*2];
+		helper(list, number, number, array, 0);
+
+		int i = 1;
+		for(String s : list)
+		{
+			System.out.println(i+ ": " + s);
+			i++;
+		}
+
 	}
 
-	private static Set<String> helper(int num) 
+	/*private static Set<String> helper(int num) 
 	{
 		Set<String> set = new HashSet();
 		if(num == 0)
@@ -46,5 +59,30 @@ public class Parens
 		String start = str.substring(0, index+1);
 		String end = str.substring(index+1);
 		return start + "()" + end;
+	}*/
+
+	private static void helper(List<String> list, int left, int right, char[] array, int index)
+	{
+		if(left < 0 || right < 0)
+		{
+			return;
+		}
+
+		// we have found a valid string
+		if(left == 0 && right == 0)
+		{
+			list.add(String.copyValueOf(array));		
+		}
+		else
+		{
+			array[index] = '(';
+			helper(list, left-1, right, array, index+1);
+
+			if(left < right) // make sure there are more left than right before adding right 
+			{
+				array[index] = ')';
+				helper(list, left, right-1, array, index+1);
+			}
+		}
 	}
 }
