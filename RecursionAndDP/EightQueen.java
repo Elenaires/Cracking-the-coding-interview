6ebc25818472fbef45e9a9e7b8585201becb90de
Lee Huey Charn <hueycharn.lee@gmail.com>
2020-03-08 22:49:42 +0800

    Qs 8.12

--- RecursionAndDP/EightQueen.java
+++ RecursionAndDP/EightQueen.java
@@ -2,11 +2,12 @@ import java.util.*;
 
 public class EightQueen {
 
-    static final int GRID = 8;
+    static final int GRID = 4;
     public static void main(String[] args){
         List<List<Integer>> results = new ArrayList<>();
-        findPosition(results, new ArrayList<Integer>(), 0 /* row */);
-        
+        //findPosition(results, new ArrayList<Integer>(), 0 /* row */);
+        bruteForce(results, new ArrayList<Integer>(), 0 /* row */);
+
         // print result
         int count = 1;
         for(List<Integer> result : results) {
@@ -19,9 +20,10 @@ public class EightQueen {
         }
     }
 
+    /** Optimal */ 
     // index of positions represents the row at which a queen is positioned
     // positions[index] represents the col at which a queen is positioned
-    private static void findPosition(List<List<Integer>> results, List<Integer> positions, 
+    /*private static void findPosition(List<List<Integer>> results, List<Integer> positions, 
                                 int row) {
         // found all positions
         if(row == GRID) {
@@ -52,6 +54,50 @@ public class EightQueen {
             int rowDistance = row - previousQueenRow;
             if(colDistance == rowDistance) {
                 return false;
+            }
+        }
+        return true;
+    }*/
+
+    /** brute force */
+    // generate all position permutations (0,1,2,3 : 0,1,3,2 : 0,2,1,3 etc)
+    // check validity of each permutation
+    private static void bruteForce(List<List<Integer>> results, List<Integer> positions, int row) {
+        if(row == GRID) {
+            if(isValid(positions)) {
+                results.add(new ArrayList<Integer>(positions));
+            }
+        }
+        else {
+            for(int col = 0; col < GRID; col++) {
+                if(!positions.contains(col)) {
+                    positions.add(col);
+                    bruteForce(results, positions, row+1);
+                    positions.remove(positions.size()-1);
+                }
+            }
+        }
+    }
+
+    private static boolean isValid(List<Integer> positions) {
+        for(int row = 1; row < positions.size(); row++) {
+            for(int previousQueenRow = 0; previousQueenRow < row; previousQueenRow++) {
+                int previousQueenCol = positions.get(previousQueenRow);
+
+                // check column
+                int col = positions.get(row);
+                if(col == previousQueenCol) {
+                    return false;
+                }
+
+                // check diagonal
+                // equal col and row distances between current position and 
+                // previous queen position means diagonal
+                int colDistance = Math.abs(previousQueenCol - col);
+                int rowDistance = row - previousQueenRow;
+                if(colDistance == rowDistance) {
+                    return false;
+                }
             }
         }
         return true;
